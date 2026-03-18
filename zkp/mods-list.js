1220dd8ce31f2ade64782a1ebe0d2d8362fa37c0
Bitf_ <cloverzzz170@gmail.com>
2026-03-18 01:40:58 -0400

    okol

--- zkp/mods-list.js
+++ zkp/mods-list.js
@@ -1,3 +1,13 @@
 const MODS_LIST = [
     "mods/pico-4th-song.html",
+    "mods/deux-sex-machina.html",
+    "mods/wheel-space.html",
+    "mods/fresher-cool-mode.html",
+    "mods/corruption-minipack.html",
+    "mods/cocoa-gf-mix.html",
+    "mods/titular-2026.html",
+    "mods/pause.html",
+    "mods/ready-set-go.html",
+    "mods/green.html",
+    "mods/2nd-half.html",
 ];
